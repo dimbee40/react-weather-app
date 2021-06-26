13670a98032ea86d6894043dd5aca2fc99bded6c
dimbee40 <78416865+dimbee40@users.noreply.github.com>
2021-06-26 21:11:46 +1000

    added search engine and html formatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
      <div className="container">
      React Weather App
-     <button>Enter</button>
+   <Weather />
      Coded by <a href="https://github.com/dimbee40/react-weather-app" >Deb</a>
     </div>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather (){
+    return(
+        <div className="Weather">
+            <form>
+                <input type="Search" placeholder="Enter a city..."
+                className="form-control" />
+                <input type="submit" value="Search" className="btn btn-primary" />
+            </form>
+            <h1>New York</h1>
+        <ul>
+            <li>Wednesday 5pm</li>
+            <li>Mostly cloudy</li>
+            </ul>
+            <div className="row">
+<div className="col-6">
+<img src="http://www.iemoji.com/view/emoji/185/animals-nature/cloud" alt= "mostly cloudy" />
+6 degrees C.
+</div>
+<div className="col-6">
+    <ul><li>Precipitation: 15%</li>
+    <li>Humidity:`5%</li>
+    <li>Wind- 13km/h</li></ul>
+</div>
+
+            </div>
+            
+            </div>
+
+            
+    )
+}
+
