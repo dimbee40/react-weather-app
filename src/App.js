6eefae46d4889e4c3376427ecc0de9efc03ced0b
dimbee40 <78416865+dimbee40@users.noreply.github.com>
2021-06-26 18:35:57 +1000

    created react app
    create react APP

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+     
+     React Weather App
+     Coded by <a href="">Deb</a>
+    </div>
+  );
+};
