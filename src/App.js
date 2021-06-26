3b81dcc57d8676db6cf7ed8ff6a60cf4c02643de
dimbee40 <78416865+dimbee40@users.noreply.github.com>
2021-06-26 20:50:08 +1000

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     
+     <div className="container">
      React Weather App
      Coded by <a href="https://github.com/dimbee40/react-weather-app" >Deb</a>
+    </div>
     </div>
   );
 };
